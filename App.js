c433ef2adb9f5023be53eecd4879712fee5021a2
James Park <jamestkd918@gmail.com>
2018-09-16 20:47:03 -0700

    #initial commit and setting up react native program

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { LineChart, Grid } from 'react-native-svg-charts';
+import SvgExample from './components/linechart';
+
+export default class App extends React.Component {
+  render() {
+    
+    return (
+      <View style={styles.container}>
+      <Text>Hello</Text>
+      <SvgExample />
+      </View>
+    );
+
+
+
+
+
+  }
+}
+
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
