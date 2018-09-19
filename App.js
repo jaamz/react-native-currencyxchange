c57176a70e9a99905bb9171efacdb462ec794d7f
James Park <jamestkd918@gmail.com>
2018-09-19 14:08:10 -0700

    #created tab navigator #updated graph component (starting to piece together with API)

--- App.js
+++ App.js
@@ -1,33 +1,57 @@
 import React from 'react';
-import Header from './components/header'
-import Transaction from './components/transaction'
 import { StyleSheet, Text, View, Picker, PickerItem } from 'react-native';
 import { LineChart, Grid } from 'react-native-svg-charts';
-import Graph from './components/graph'
 import { Dropdown } from 'react-native-material-dropdown';
+import { createBottomTabNavigator } from 'react-navigation';
+
+import { Ionicons } from '@expo/vector-icons';
+
+import Header from './components/header'
+import Graph from './components/graph'
 import Mainpage from './components/mainpage';
-export default class App extends React.Component {
-  render() {
-
-    return (
-      <View
-        style={styles.container}>
-        {/* <View>
-          <Header />
-        </View> */}
-        {/* <Graph /> */}
-         <Mainpage />
-        {/* <Transaction /> */}
-      </View>
-    );
+import Transaction from './components/transaction'
 
 
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: Mainpage,
+    Graph: Graph,
+    Transactions: Transaction
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Graph') {
+          iconName = `ios-trending-up${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Transactions') {
+          iconName = `ios-paper${focused ? '' : '-outline'}`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'green',
+      inactiveTintColor: 'gray',
+    },
+  });
 
 
+// {/* <View>
+//   <Header />
+// </View> */}
+//  {/* <Mainpage /> */}
+// {/* <Transaction /> */}
 
-  }
-}
+const App = () => <TabNavigator />
 
+export default App;
 
 
 
--- components/graph.js
+++ components/graph.js
@@ -1,23 +1,53 @@
-import React from 'react'
-import { LineChart, YAxis, Grid } from 'react-native-svg-charts'
-import { View, Text, TouchableOpacity } from 'react-native'
-
+import React from 'react';
+import { LineChart, YAxis, Grid } from 'react-native-svg-charts';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { dropDownSelection } from './constant/index';
+import { Dropdown } from 'react-native-material-dropdown';
+import axios from'axios';
 class Graph extends React.PureComponent {
     state = {
-        
+        data:[1, 1.3, 1.5, 1.6, .9, 1.5, 1.3, 1.6],
+        currency1:'USD',
+        currency2:'JPY',
     }
 
-    render() {
+    componentDidMount() {
+        axios.get(`http://localhost:3000/${this.state.currency}?duration=${this.state.moneyValue}`)
+            .then(res => {
+                // console.log(res.data[0].rates);
+                this.setState({
+                    data: res.data[0].rates
+                })
+            })
+    }
 
-        const data = [1, 1.3, 1.5, 1.6, .9, 1.5, 1.3, 1.6]
+
+    render() {
 
         const contentInset = { top: 20, bottom: 20 }
 
         return (
             <View>
+            <View style={{flexDirection:'row'}}>
+                <Dropdown
+                    containerStyle={styles.dropDownStyle}
+                    label='Main Currency'
+                    data={dropDownSelection}
+                    onChangeText={text => { this.setState({ currency1: text }) }}
+                    value={this.state.currency1}
+                />
+
+                <Dropdown
+                    containerStyle={styles.dropDownStyle}
+                    label='Currency Compared'
+                    data={dropDownSelection}
+                    onChangeText={text => { this.setState({ currency2: text }) }}
+                    value={this.state.currency2}
+                />    
+            </View>
             <View style={{ height: 200, flexDirection: 'row' }}>
                 <YAxis
-                    data={data}
+                    data={this.state.data}
                     contentInset={contentInset}
                     svg={{
                         fill: 'grey',
@@ -28,7 +58,7 @@ class Graph extends React.PureComponent {
                 />
                 <LineChart
                     style={{ flex: 1, marginLeft: 16 }}
-                    data={data}
+                    data={this.state.data}
                     svg={{ stroke: 'rgb(134, 65, 244)' }}
                     contentInset={contentInset}>
                     <Grid />
@@ -60,3 +90,46 @@ class Graph extends React.PureComponent {
 }
 
 export default Graph;
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+    textInput: {
+        width: 50,
+        height: 30,
+        textAlign: 'center',
+        borderWidth: 1,
+        borderColor: 'black',
+
+    },
+    dropDownStyle: {
+        width: 120,
+
+    },
+    buttonStyle: {
+        borderColor: 'black',
+        borderWidth: .5,
+        width: 30,
+        height: 30,
+
+
+
+    },
+    mapStyle: {
+        borderWidth: 1,
+        borderColor: 'black',
+        height: 40,
+        width: 300,
+        alignItems: 'center',
+        justifyContent: 'center',
+
+
+    },
+    buttonContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
+
